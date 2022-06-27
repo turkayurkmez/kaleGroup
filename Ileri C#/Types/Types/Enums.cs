a6f1f49fdf03a7d4913c5816fe15cad974a8393d
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-06-27 11:03:10 +0300

    folder structure has changed

---
+++ Ileri C#/Types/Types/Enums.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    public enum OrderState
+    {
+        Pending,
+        Success,
+        Failed
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+
+    }
+
+    public enum ErkekAkrabalikTipleri
+    {
+        Abi = 1,
+        Ogul = 2,
+        Baba = 4,
+        Amca = 8,
+        Dayı = 16,
+        Dede = 32
+
+    }
+
+
+
+
+
+}
---
+++ Ileri C#/Types/Types/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Types;
+
+Console.WriteLine("Hello, World!");
+
+byte value = 255;
+byte minimum = 3;
+
+//checked
+//{
+//    byte total = (byte)(value + minimum);
+//    Console.WriteLine(total);
+//}
+
+//C# Tipi
+string companyName = "Kale";
+//.NET Tipi
+String sirketAdi = "Kale";
+
+int x = 8;
+Int32 number = x;
+var direction = Direction.Left;
+
+switch (direction)
+{
+    case Direction.Up:
+        break;
+    case Direction.Down:
+        break;
+    case Direction.Left:
+        break;
+    case Direction.Right:
+        break;
+    default:
+        break;
+}
+
+Console.WriteLine((int)direction);
+ErkekAkrabalikTipleri erkek = ErkekAkrabalikTipleri.Ogul | ErkekAkrabalikTipleri.Abi | ErkekAkrabalikTipleri.Baba | ErkekAkrabalikTipleri.Dayı | ErkekAkrabalikTipleri.Amca;
+
+Console.WriteLine(erkek);
