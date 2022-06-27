841c708dac395d7eae88bedd645fc88826fcfb6c
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-06-27 18:07:51 +0300

    last touch

---
+++ Ileri C#/Delegates/Delegates/FilteringObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public static class FilteringObject
+    {
+       //public delegate bool criteriaDelegate(int number); 
+       public static int[] Filter(int[] values, Func<int, bool> criteria)
+        {
+            List<int> filteredResult = new List<int>();
+            foreach (var item in values)
+            {
+                if (criteria(item))
+                {
+                    filteredResult.Add(item);
+                }
+            }
+
+            return filteredResult.ToArray();
+        }
+
+
+    }
+}
--- Ileri C#/Delegates/Delegates/Program.cs
+++ Ileri C#/Delegates/Delegates/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Delegates;
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Console.WriteLine(".net 1.1");
+var dotnet1 = FilteringObject.Filter(numbers, isEven);
+show(dotnet1);
+Console.WriteLine("-------------------");
+Console.WriteLine(".net 2.0");
+
+var dotnet2 = FilteringObject.Filter(numbers, delegate (int number) { return number % 3 == 0; });
+show(dotnet2);
+Console.WriteLine("-------------------");
+
+Console.WriteLine(".net 3.0+");
+var dotnet3 = FilteringObject.Filter(numbers, number =>  number % 4 == 0);
+alternateShow(dotnet3);
+Console.WriteLine("-------------------");
+
+
+
+
+void show(int[] numbers)
+{
+    foreach (var item in numbers)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+void alternateShow(int[] numbers)
+{
+    numbers.ToList().ForEach(number => Console.WriteLine(number));
+}
+
+//çift sayıları filtrelemek istiyoruz.
+
+
+//static int[] filter(int[] values)
+//{
+//    List<int> filteredResult = new List<int>();
+//    foreach (var item in values)
+//    {
+//        if (isEven(item))
+//        {
+//            filteredResult.Add(item);
+//        }
+//    }
+
+//    return filteredResult.ToArray();
+//}
+
+static bool isEven(int number)
+{
+    return number % 2 == 0;
+}
+
+static bool isOdd(int number)
+{
+    return number % 2 == 1;
+}
